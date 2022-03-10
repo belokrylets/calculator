f8ec129ca8da50cc3d2d7589ae2cd73ba93f9158
Sergey Belokrylets <s.belokrylets@gmail.com>
2022-03-10 12:45:03 +0300

    создан и запущен файл calculator.ts, внесены изенения в файл formulas.js

--- src/calculations/calculator.ts
+++ src/calculations/calculator.ts
@@ -1,34 +1,32 @@
-import { sum, subtraction, multiplication, division, root, percent, sumPercent, subtractionPercent } from './formulas';
+import { sum, subtraction, multiplication, division, root, percent, sumPercent, subtractionPercent } from './src/calculations/formulas';
 
+const calculationSigns:string[] = ['+', '-', '×', '/', '%', '√'];
 
-var calculationSigns = ['+', '-', '×', '/', '%', '√'];
-function arrayСonstructor(str) {
-    var stringWithSpaces = '';
-    for (var i = 0; i < str.length; i++) {
+function arrayСonstructor(str: string):string[] {   //преобразует строку из дисплея в массив
+    let stringWithSpaces = '';
+    for (let i = 0; i < str.length; i++) {
         if (calculationSigns.includes(str[i])) {
-            stringWithSpaces += " ".concat(str[i], " ");
-        }
-        else {
+            stringWithSpaces += ` ${str[i]} `;
+        } else {
             stringWithSpaces += (str[i]);
         }
     }
     return stringWithSpaces.split(' ');
 }
-function emptyCellRemover(arr) {
-    var result = [];
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var item = arr_1[_i];
+function emptyCellRemover(arr: string[]):string[] {   // удаляет из массва пустые ячейки
+    const result = [];
+    for (const item of arr) {
         if (item !== undefined) {
             result.push(item);
         }
     }
     return result;
 }
-function calculator(str) {
-    var commaSubstitution = str.replace(/,/gi, '.');
-    var arr = arrayСonstructor(commaSubstitution);
+function calculator(str: string):number {
+    const commaSubstitution: string = str.replace(/,/gi, '.');
+    var arr: number[] | string[] = arrayСonstructor(commaSubstitution);
     if (!!arr.includes('√')) {
-        arr[0] = `${root(arr[0])}`;
+        arr[0] = root(arr[0]);
         delete arr[1];
         delete arr[2];
         arr = emptyCellRemover(arr);
@@ -39,8 +37,7 @@ function calculator(str) {
             delete arr[1];
             delete arr[2];
             arr = emptyCellRemover(arr);
-        }
-        else {
+        } else {
             (arr[1] === '-') ? arr[0] = `${subtractionPercent(arr[0], arr[2])}` : arr[0] = `${sumPercent(arr[0], arr[2])}`;
             delete arr[1];
             delete arr[2];
@@ -50,14 +47,14 @@ function calculator(str) {
         }
     }
     while (!!arr.includes('/') || !!arr.includes('×')) { //цикл работает пока в массиве есть умножение и деление
-        for (var i = 0; i <= arr.length; i++) {
+        for (let i = 0; i <= arr.length; i++) {
             if (arr[i] === '×') {
                 arr[i] = `${multiplication(arr[i - 1], arr[i + 1])}`;
                 delete arr[i - 1];
                 delete arr[i + 1];
                 arr = emptyCellRemover(arr);
-            }
-            else if (arr[i] === '/') {
+
+            } else if (arr[i] === '/') {
                 arr[i] = `${division(arr[i - 1], arr[i + 1])}`;
                 delete arr[i - 1];
                 delete arr[i + 1];
@@ -66,14 +63,14 @@ function calculator(str) {
         }
     }
     while (arr.length > 1) { // цикл работает пока не выполняться все вычетания и сложения
-        for (var i = 0; i <= arr.length; i++) {
+        for (let i = 0; i <= arr.length; i++) {
             if (arr[i] === '+') {
                 arr[i] = `${sum(arr[i - 1], arr[i + 1])}`;
                 delete arr[i - 1];
                 delete arr[i + 1];
                 arr = emptyCellRemover(arr);
-            }
-            else if (arr[i] === '-') {
+
+            } else if (arr[i] === '-') {
                 arr[i] = `${subtraction(arr[i - 1], arr[i + 1])}`;
                 delete arr[i - 1];
                 delete arr[i + 1];
@@ -84,5 +81,3 @@ function calculator(str) {
     return Number(arr[0]);
 }
 export default calculator;
-
-
--- src/calculations/formulas.js
+++ src/calculations/formulas.js
@@ -1,35 +1,25 @@
-"use strict";
-exports.__esModule = true;
-exports.subtractionPercent = exports.sumPercent = exports.percent = exports.root = exports.division = exports.multiplication = exports.subtraction = exports.sum = void 0;
-function sum(a, b) {
+
+export function sum(a, b) {
     return Number(a) + Number(b);
 }
-exports.sum = sum;
-function subtraction(a, b) {
+export function subtraction(a, b) {
     return Number(a) - Number(b);
 }
-exports.subtraction = subtraction;
-function multiplication(a, b) {
+export function multiplication(a, b) {
     return Number(a) * Number(b);
 }
-exports.multiplication = multiplication;
-function division(a, b) {
+export function division(a, b) {
     return Number(a) / Number(b);
 }
-exports.division = division;
-function root(a) {
+export function root(a) {
     return Math.sqrt(Number(a)).toFixed(2);
 }
-exports.root = root;
-function percent(a) {
+export function percent(a) {
     return Number(a) / 100;
 }
-exports.percent = percent;
-function sumPercent(a, b) {
+export function sumPercent(a, b) {
     return Number(a) + ((Number(a) * Number(b)) / 100);
 }
-exports.sumPercent = sumPercent;
-function subtractionPercent(a, b) {
+export function subtractionPercent(a, b) {
     return Number(a) - ((Number(a) * Number(b)) / 100);
 }
-exports.subtractionPercent = subtractionPercent;
