f5efca0f784ec14298eeff193c53fa686eac7c8b
Sergey Belokrylets <s.belokrylets@gmail.com>
2022-03-10 13:42:19 +0300

    переименоан App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import MyButtons from './components/MyButtons';
+import './App.css';
+import calculator from './calculations/calculator';
+
+function App() {
+  let [display, setDisplay] = useState('');
+  let [result, setResult] = useState(0);
+  function setFormula(arg) {
+    setDisplay(display + arg);
+  }
+  function clearFormula() {
+    setDisplay(display = '');
+    setResult(result = 0);
+  }
+  function displeyResult() {
+    setResult(result = calculator(display));
+    setDisplay(display = calculator(display));
+  }
+
+  return (
+    <div className="calculator">
+      <div className="calculation">{display}</div>
+      <div className="result">{result}</div>
+      <div className="line" />
+      <div className="buttons">
+        <MyButtons setFormula={setFormula} clearFormula={clearFormula} displeyResult={displeyResult} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
